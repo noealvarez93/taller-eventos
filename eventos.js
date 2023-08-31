f9d68772d0118a28329e114da3610e9399d4cffa
noealvarez93 <noeliaalvareztorres@gmail.com>
2023-08-31 20:59:53 -0300

    contenedorDiv dice Hola! soy el div, saludarBoton dice Hola

---
+++ eventos.js
@@ -0,0 +1,11 @@
+const saludarButton = document.getElementById("saludarButton");
+
+saludarButton.addEventListener("click", function(event) {
+    alert("Hola!");
+});
+
+const contenedorDiv = document.getElementById("contenedorDiv");
+
+contenedorDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
